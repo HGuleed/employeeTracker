196c96a444f2662775a885fe588260569f66a549
Hodan <you@example.com>
2022-01-20 06:44:39 -0500

    added starter files and schema & seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (department_name)
+VALUES
+('Human Resources'),
+('Finance'),
+('Sales'),
+('Marketing');
+
+INSERT INTO roles( title, salary)
+VALUES 
+('HR Manager', 65,000),
+('FA Manager', 60,000),
+('MA Manager', 55,000),
+('SR Manager', 45,000)
+('Human Resources Representative', 55,000),
+('Financial Advisor', 50,000),
+('Marking Agent', 45,000),
+('Sale Representative', 35,000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES 
+('Joseph','Beck'),
+('Tyler','Mosser'),
+('Bey', 'Waters'),
+('Alex', 'Black'),
+('Jennifer','Jones'),
+('John', 'Smith'),
+('Sarah', 'Steven'),
+('Joselyn','Johnson'),
+('Jim','Beam'),
+('Stephanie','Taylor'),
+('Jasmine','Garcia'),
+('Ashley','Baker');
